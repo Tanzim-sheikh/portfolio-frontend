744fd28194551950daa096cf2fdfe4bb38829912
Tanzim Sheikh <tanzimsheikh68@gmail.com>
2025-10-28 11:03:49 +0530

    adding E in evenza

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -74,7 +74,7 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import { FiExternalLink, FiGithub } from 'react-icons/fi';
-import evenza from "../assets/Evenzap.png";
+import Evenza from "../assets/Evenzap.png";
 import Instachat from "../assets/InstaChat2.png"
 import streetSpices from "../assets/project1.png"
 
